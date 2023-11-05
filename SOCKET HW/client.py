fec935c78d8f33228fcfb2bc0d63815ef1c4df5d
Suzanne Nolba <113120098+L12Nolb@users.noreply.github.com>
2023-11-05 16:53:26 -0600

    tcp chatroom

---
+++ SOCKET HW/client.py
@@ -0,0 +1,20 @@
+'''
+author: Suzanne Nolba
+version: 1.4
+course: CSCE 3530
+This file will be our client sending PING to a designed Server
+It will also get a response from the server
+'''
+import socket
+
+
+for i in range(10):
+    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    client.settimeout(1)
+    addresse = (socket.gethostbyname(socket.gethostname()), 8008)
+    client.sendto(b'PING', addresse)
+    try:
+        response, server = client.recvfrom(1024)
+        print('{:^2} : sent PING... received {}'.format(i+1, response))
+    except socket.timeout:
+        print('{:^2} : sent PING... Timed Out'.format(i+1))
---
+++ SOCKET HW/server.py
@@ -0,0 +1,23 @@
+'''
+author: Suzanne Nolba
+version: 1.2
+course: CSCE 3530
+This be the server receiving any request from clients
+It wil send back to the client a PONG but might drop it
+'''
+import random
+import socket
+
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server.bind(('', 8008))
+print('sln0098 server connected')
+print('[server] : ready to accept data...')
+
+while 1:
+    seed = random.randint(0, 10)
+    message, address = server.recvfrom(1024)
+    if seed >= 3:
+        print(f'[client] : {message}')
+        server.sendto(b'PONG', address)
+    else:
+        print('[server] : packet dropped')
